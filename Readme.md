bdecce6831b7c7e0dffd458df0ef72ae06061fb3
yutent <yutent.io@gmail.com>
2023-03-06 15:24:17 +0800

    增加示例组件

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# 9th/wcui
+9号UI组件库, 基于web components开发。面向下一代的UI组件库
+
+
+### 开发环境
+
+  + vscode  开源编辑器
+    - `Prettier`插件, 格式化代码用
+    - `string-html-css`, 可高亮显示js中的`html/css`文本, 并支持`emmet`
+    - `simple http`, 可快速配置http服务器,支持history路由
+    - ...
+
+  + @ninejs/core  针对`web components`的核心封装库, 以数据驱动, 可以更方便的开发wc组件
+
+  + @ninejs/cli   开发9号UI组件的构建工具
---
+++ src/card/index.js
@@ -0,0 +1,74 @@
+/**
+ * {卡片组件}
+ * @author yutent<yutent.io@gmail.com>
+ * @date 2023/03/06 15:17:25
+ */
+
+import { css, html, Component } from '@ninejs/core'
+
+class Card extends Component {
+  static props = {
+    header: {
+      type: String,
+      default: ''
+    }
+  }
+
+  static styles = css`
+    :host {
+      display: flex;
+      border-radius: 3px;
+    }
+
+    .card-box {
+      display: flex;
+      flex-direction: column;
+      position: relative;
+      width: 100%;
+      border: 1px solid var(--color-plain-2);
+      border-radius: 3px;
+      background: #fff;
+      color: var(--color-dark-1);
+      transition: box-shadow 0.2s ease-in-out;
+      box-shadow: 0 0 12px rgba(0, 0, 0, 0.12);
+    }
+    .card-box .header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 100%;
+      min-height: 52px;
+      padding: var(--card-padding, 8px 16px);
+      border-bottom: 1px solid var(--color-plain-2);
+      font-size: 16px;
+      user-select: none;
+    }
+
+    .card-box .content {
+      flex: 1;
+      min-height: 64px;
+      padding: var(--card-padding, 8px 16px);
+      font-size: 14px;
+      color: var(--color-dark-1);
+    }
+
+    :host([shadow='never']) .card-box,
+    :host([shadow='hover']) .card-box {
+      box-shadow: none;
+    }
+    :host([shadow='hover']:hover) .card-box {
+      box-shadow: 0 0 12px rgba(0, 0, 0, 0.12);
+    }
+  `
+
+  render() {
+    return html`
+      <div class="card-box">
+        <div class="header">${this.header && '<slot name="header" />'}</div>
+        <div class="content"><slot /></div>
+      </div>
+    `
+  }
+}
+
+customElements.define('wc-card', Card)
